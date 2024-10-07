8a13c7f4e901554d1aca2ca3cb74a4673c8cb637
Jami Karimkulov <jami.karimkul@gmail.com>
2024-10-07 12:50:01 -0400

    Add reading file from file system

---
+++ modul-4/02-reading-file/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+class Data {
+    read(fileUrl) {
+        return fs.readFileSync(fileUrl, "utf-8");
+    }
+    parse(content) {
+        return content.splite("/n");
+    }
+    parseRow(row) {
+        return row.splite(",");
+    }
+    format(fileUrl) {
+        const content = this.read(fileUrl);
+        const all = this.parse(content);
+        const header = this.parseRow(all[0]);
+        const rows = all.slice(1);
+
+        const formattedData = rows.map((row) => {
+            const personArr = this.parseRow(row);
+            const personObj = {};
+            for (let i = 0; i < header.length; i++) {
+                const label = header[i];
+                const value = personArr[i];
+                personObj[label] = value;
+            }
+
+            return personObj;
+        });
+
+        return formattedData;
+    }
+}
+
+const dataProcessor = new Data();
+const result = dataProcessor.format("./02-reading-file/data.csv");
+console.log(result);
